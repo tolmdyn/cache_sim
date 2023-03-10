258df418f9924aa6726e36211628cc1561ff29ee
tolmdyn <tolmdyn@gmail.com>
2023-03-10 15:29:00 +0000

    cache implementation

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
 pub fn main() {
-    process_address(0x7ff0005c8, 4, 8, 1);
-    process_address(26, 4, 4, 1);
+    //sim::process_address(0x7ff0005c8, 4, 8, 1);
+    //sim::process_address(26, 4, 4, 1);
 
+    /*
+    let mut cache = sim::Cache::new(4, 4, 4);
+
+    println!("{:?}", cache);
+
+    cache.operate_cache(26);
+    cache.operate_cache(526);
+    cache.operate_cache(26);
+    //cache.get_stats();
+
+    println!("{:?}", cache);
+    */
+
+    let mut direct_cache = sim::Cache::new(2, 4, 2);
+
+    direct_cache.operate(0b00001111); //miss
+    direct_cache.operate(0b01011111); //miss
+    direct_cache.operate(0b10101111); //miss
+    direct_cache.operate(0b11111111); //miss
+    direct_cache.operate(0b100001111); //miss
+
+    direct_cache.operate(0b11111111); //hit
+
+    direct_cache.operate(0b101100001111); //full
+
+    //println!("{:?}", direct_cache);
+    println!("{}", direct_cache);
 }
 
 
-struct Line {
-    addr: u64,
-    block: u64,
-    valid: bool,
-    tag: u64,
-    //last_access
-}
 
 
 
 
-fn process_address(addr:u64, set_bits:u64, block_bits:u64, num_lines:u32) { //->line {}
-    println!("S:{set_bits}, b:{block_bits}, E:{num_lines}");
-    println!("0x{:x} b{:0>64b}", addr, addr);
 
-    //let blockmask:u64 = u64::pow(2, block_bits.try_into().unwrap())-1;
 
-    let blockmask:u64 = (1 << block_bits) -1;
-    let block:u64 = addr & blockmask;
-
-    //let setmask:u64 = u64::pow(2, (set_bits + block_bits).try_into().unwrap())-1;
-    let setmask:u64 = (1 << set_bits + block_bits) -1;
-    let set:u64 = (addr & setmask) >> block_bits;
-
-    println!("blockmask:{:b}, block:{:b}\nsetmask:{:b}, set:{:b}", blockmask, block, setmask, set);
-}
 
