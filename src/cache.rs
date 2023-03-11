c6022fa64af942a06afd0f0bd7672fa247ecaae6
tolmdyn <tolmdyn@gmail.com>
2023-03-11 13:39:32 +0000

    Rewrote operate with Results

--- src/cache.rs
+++ src/cache.rs
@@ -8,6 +8,7 @@ use std::fmt;
     tag: u64,
     //last_access
 }*/
+
 #[derive(PartialEq)]
 pub enum CacheResult {
     Hit,
@@ -15,26 +16,25 @@ pub enum CacheResult {
     Eviction,
 }
 #[derive(PartialEq)]
+#[allow(dead_code)]
 pub enum CacheInstruction {
     LOAD,
     STORE,
     MODIFY,
 }
 
+#[allow(dead_code)]
 #[derive(Debug)]
 struct Address {
     addr: u64,
-    _tag: u64,
+    tag: u64,
     set: u64,
-    _block: u64,
+    block: u64,
 }
 
 #[derive(Debug)]
 struct Set {
     lines: VecDeque<u64>, //just the tag or full Address?
-                          //lines: HashMap<u64, &Line>,
-                          //queue: VecDeque<Line>
-                          //other things
 }
 
 impl Set {
@@ -47,7 +47,8 @@ impl Set {
 
 #[derive(Debug)]
 pub struct Cache {
-    sets: Vec<Set>,
+    //sets: Vec<Set>,
+    sets: Box<[Set]>,
 
     set_bits: u64,   //(s)
     block_bits: u64, //(b)
@@ -69,9 +70,10 @@ impl Cache {
         }
 
         //println!("{} {}", set_num, new_sets.len());
+        let set_box = new_sets.into_boxed_slice(); //cast it into a boxed slice as we don't need to resize it anymore
 
         Self {
-            sets: new_sets,
+            sets: set_box,
             set_bits,
             block_bits,
             num_lines,
@@ -81,22 +83,23 @@ impl Cache {
         }
     }
 
-    pub fn operate(&mut self, addr: u64) -> CacheResult {
+    pub fn operate(&mut self, addr: u64) -> Result<CacheResult, String> {
         let address = self.process_address(addr);
         //println!("{:?}", address);
 
-        if self.check_hit(&address) {
+        if self.check_hit(&address).unwrap() {
             //check if address is in cache and if it is move to back of queue and update "hit"
-            return CacheResult::Hit;
-        } else if self.check_free(&address) {
+            Ok(self.update(&address)?) //return CacheResult::Hit;
+        } else if self.check_free(&address).unwrap() {
             //if there is space in the cache then add it to cache, put in back of queue and update "miss"
-            self.insert(&address);
-            return CacheResult::Miss;
+            Ok(self.insert(&address)?) ////return CacheResult::Miss;
+            
         } else {
             //if cache is full then evict, then add it to cache and update "full"
-            self.evict(&address);
-            return CacheResult::Eviction;
+            Ok(self.evict(&address)?) //return CacheResult::Eviction;
         }
+
+        
     }
     /*
     pub fn instruction(&mut self, addr: u64, inst: CacheInstruction) -> &[CacheResult] {
@@ -117,92 +120,72 @@ impl Cache {
     fn process_address(&self, addr: u64) -> Address {
         //println!("0x{:x} b{:0>64b}", addr, addr);
 
-        //let blockmask:u64 = u64::pow(2, block_bits.try_into().unwrap())-1;
         let blockmask: u64 = (1 << self.block_bits) - 1;
         let block: u64 = addr & blockmask;
 
-        //let setmask:u64 = u64::pow(2, (set_bits + block_bits).try_into().unwrap())-1;
         let setmask: u64 = (1 << self.set_bits + self.block_bits) - 1;
         let set: u64 = (addr & setmask) >> self.block_bits;
 
         let tag: u64 = addr >> (self.block_bits + self.set_bits);
-        //println!("blockmask:{:b}, block:{:b}\nsetmask:{:b}, set:{:b}, tag{:b}\n", blockmask, block, setmask, set, tag);
 
         Address {
             addr,
-            _tag: tag,
-            _block: block,
+            tag,
+            block,
             set,
         }
     }
-    /* CHANGE ALL THIS TRUE FALSE NONSENSE TO SOMETHING MORE RUST-Y (RESULT?) */
-    fn check_hit(&mut self, addr: &Address) -> bool {
-        if let Some(set) = self.sets.get_mut(addr.set as usize) {
-            if set.lines.contains(&addr.addr) {
-                self.hit += 1;
-                //update access
-                let i = set.lines.iter().position(|&x| x == addr.addr);
-                set.lines.remove(i.unwrap());
-                set.lines.push_back(addr.addr);
-                return true;
-            }
-        }
-        false
-    }
 
-    /*
-    fn check_miss(&mut self, addr: &Address) -> bool {
-        if let Some(set) = self.sets.get_mut(addr.set as usize) {
-            if &set.lines.len() < &set.lines.capacity() {
-                set.lines.push_back(addr.addr);
-                self.miss += 1;
-                return true;
-            }
-        }
-        false
-    }*/
-
-    fn check_free(&self, addr: &Address) -> bool {
+    fn check_hit(&self, addr: &Address) -> Result<bool, String> {
+        //check if item is in cache
         if let Some(set) = self.sets.get(addr.set as usize) {
-            return &set.lines.len() < &set.lines.capacity();
+            return Ok(set.lines.contains(&addr.addr))
         }
-        panic!("Problem checking for space")
+        Err("Problem checking for hit".to_string())
     }
 
-    fn insert(&mut self, addr: &Address) -> bool {
+    fn update(&mut self, addr: &Address) -> Result<CacheResult, String> {
+        //if the item is in cache then update the LRU
+
+        if let Some(set) = self.sets.get_mut(addr.set as usize) {
+            self.hit += 1;
+            let index = set.lines.iter().position(|&x| x == addr.addr);
+            set.lines.remove(index.unwrap());
+            set.lines.push_back(addr.addr);
+            return Ok(CacheResult::Hit);
+        }
+
+        Err("Cache does not contain address.".to_string())
+    }
+
+    fn check_free(&self, addr: &Address) -> Result<bool, String> {
+        if let Some(set) = self.sets.get(addr.set as usize) {
+            return Ok(&set.lines.len() < &set.lines.capacity())
+        }
+        Err("Problem checking for a free space".to_string())
+    }
+
+    fn insert(&mut self, addr: &Address) -> Result<CacheResult, String> {
         if let Some(set) = self.sets.get_mut(addr.set as usize) {
             set.lines.push_back(addr.addr);
             self.miss += 1;
-            return true;
+            return Ok(CacheResult::Miss)
         }
-        false
+        Err("Cannot insert into cache".to_string())
     }
 
-    fn evict(&mut self, addr: &Address) -> bool {
+    fn evict(&mut self, addr: &Address) -> Result<CacheResult, String> {
         if let Some(set) = self.sets.get_mut(addr.set as usize) {
             if &set.lines.len() == &set.lines.capacity() {
                 set.lines.pop_front();
                 set.lines.push_back(addr.addr);
                 self.evict += 1;
 
-                return true;
+                return Ok(CacheResult::Eviction)
             }
         }
-        false
+        Err("Cannot evict from cache".to_string())
     }
-    /*
-    fn check_evict(&mut self, addr: &Address) -> bool {
-        if let Some(set) = self.sets.get_mut(addr.set as usize) {
-            if &set.lines.len() == &set.lines.capacity() {
-                set.lines.pop_front();
-                set.lines.push_back(addr.addr);
-                self.evict += 1;
-
-                return true;
-            }
-        }
-        false
-    }*/
 }
 
 impl fmt::Display for Cache {
